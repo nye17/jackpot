a9eb2b902e9f69e59809d21f2960765f1017f344
Ying Zu <zuying@gmail.com>
2020-12-16 11:21:10 +0800

    does not work well for mac

--- sync_my_clock_before_click.sh
+++ sync_my_clock_before_click.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-while [ $(date "+%H") -lt 20 ]; do
+while [ $(date "+%H") -lt 24 ]; do
     echo "Time Now: `date +%H:%M:%S`"
     echo "Sleeping for 10 seconds"
     sleep 10
     sudo sntp -sS ntp0.cornell.edu
+    # sudo sntp -sS time.nist.gov
 done
